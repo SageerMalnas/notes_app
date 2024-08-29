091f87ce77836379a54de51bd025d66aa9402dfa
SageerMalnas <sageermalnas78@gmail.com>
2024-08-29 09:12:00 +0530

    style changes in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import './Header.css';
 export default function Header({ selectedGroup }) {
     return (
         <div className="header">
-            <div className='group-icon'>{getGroupInitials(selectedGroup.name)}</div>
+            <div className='icon'>{getGroupInitials(selectedGroup.name)}</div>
             <h1>{selectedGroup ? selectedGroup.name : 'Select a Group'}</h1>
         </div>
     );
--- src/utils/dateUtils.js
+++
@@ -1,9 +0,0 @@
-export function formatDate(date) {
-    return new Intl.DateTimeFormat('en-US', {
-        year: 'numeric',
-        month: 'short',
-        day: '2-digit',
-        hour: '2-digit',
-        minute: '2-digit'
-    }).format(new Date(date));
-}
